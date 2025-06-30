7db2cd591161d907e88949308c9c1d149dda9562
HamzaOubenslah <oubensalhhamza@gmail.com>
2025-06-30 17:55:25 +0100

    Change babel To CommonJs

--- backend/babel.config.js
+++ backend/babel.config.js
@@ -1,33 +1,6 @@
-// babel.config.js (ESM + CommonJS support for Jest)
-export default {
+module.exports = {
   presets: [
-    "@babel/preset-env", // Transpile modern JS to older versions (CommonJS & ESM)
-  ],
-  plugins: [],
-  overrides: [
-    {
-      test: ['./src/**/*', './backend/**/*'], // This depends on your project structure
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: { node: 'current' }, // Ensure it's targeting Node.js version
-            modules: 'commonjs', // Transpile ESM to CommonJS for Jest (important)
-          },
-        ],
-      ],
-    },
-    {
-      test: ['./src/**/*.mjs', './backend/**/*.mjs'], // Optional, if you have .mjs files (ESM)
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: { node: 'current' }, // Ensure it's targeting Node.js version
-            modules: false, // Don't transpile to CommonJS, keep as ESM
-          },
-        ],
-      ],
-    },
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
   ],
 };
