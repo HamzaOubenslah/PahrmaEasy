43e84283681f48573b5df41c7ba4bf840b8986fc
HamzaOubenslah <oubensalhhamza@gmail.com>
2025-06-30 16:50:49 +0100

    Updating The Configuration Of Jest In Package.json

--- backend/babel.config.js
+++ backend/babel.config.js
@@ -1,19 +1,18 @@
+// babel.config.js (ESM version)
 export default {
   presets: [
     "@babel/preset-env", // Transpile modern JS to older versions
   ],
-  plugins: [
-    // Add any necessary plugins here (optional)
-  ],
+  plugins: [],
   overrides: [
     {
-      test: ["./src/**/*", "./backend/**/*"], // This depends on your project structure
+      test: ['./src/**/*', './backend/**/*'], // This depends on your project structure
       presets: [
         [
-          "@babel/preset-env",
+          '@babel/preset-env',
           {
-            targets: { node: "current" }, // Ensure it's targeting Node.js version
-            modules: "commonJS", // This is key for Jest to work with CommonJS modules
+            targets: { node: 'current' }, // Ensure it's targeting Node.js version
+            modules: 'module', // This is key for Jest to work with CommonJS modules
           },
         ],
       ],
