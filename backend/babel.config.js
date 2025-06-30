2ad978ba6f8e37834a88794c92650bfda7534b03
HamzaOubenslah <oubensalhhamza@gmail.com>
2025-06-30 16:37:54 +0100

    Update CI Test YML File And Test A Basic Jest Code

--- backend/babel.config.js
+++ backend/babel.config.js
@@ -1,5 +1,22 @@
-export default {
+module.exports = {
   presets: [
     "@babel/preset-env", // Transpile modern JS to older versions
+  ],
+  plugins: [
+    // Add any necessary plugins here (optional)
+  ],
+  overrides: [
+    {
+      test: ['./src/**/*', './backend/**/*'], // This depends on your project structure
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: { node: 'current' }, // Ensure it's targeting Node.js version
+            modules: 'commonjs', // This is key for Jest to work with CommonJS modules
+          },
+        ],
+      ],
+    },
   ],
 };
